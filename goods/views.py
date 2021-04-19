fe584b218dafff9481bc9f38c6f3067215523938
root <395320875@qq.com>
2021-04-19 23:39:58 +0800

    创建首页--罗

--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from django.views import View
 # Create your views here.
+from goods import models
+from goods.models import *
+
+
+def index(request,cid=2):
+     cid=int(cid)
+     # 查询所有类别信息
+     cate_list=Category.objects.all().order_by('id')
+     #查询当前类别下的所有商品信息
+     good_list=Goods.objects.filter(category_id=2).order_by('id')
+     return render(request,"index.html",{'categorys':cate_list,'goodslist':'good_list','currentcid':cid})
