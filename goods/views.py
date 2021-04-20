56984ccf50b5b0d1c95cf20324aaeb5a54dd9311
root <395320875@qq.com>
2021-04-20 13:32:39 +0800

    首页调整--一

--- goods/views.py
+++ goods/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
+from django.db import models
 from django.views import View
 # Create your views here.
 from goods import models
@@ -9,7 +9,8 @@ from goods.models import *
 def index(request,cid=2):
      cid=int(cid)
      # 查询所有类别信息
-     cate_list=Category.objects.all().order_by('id')
+     categorys=Category.objects.all().order_by('id')
+     print(Category.cname)
      #查询当前类别下的所有商品信息
-     good_list=Goods.objects.filter(category_id=2).order_by('id')
-     return render(request,"index.html",{'categorys':cate_list,'goodslist':'good_list','currentcid':cid})
+     goodsList=Goods.objects.filter(category_id=2).order_by('id')
+     return render(request,"index.html",{'categorys':categorys,'goodsList':'goodsList','currentCid':cid})
